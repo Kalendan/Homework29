c335fd2b8d9c600ad6d74c55ad288eda323c9e16
nadja <nadja.kalenda31@gmail.com>
2024-06-11 17:28:03 +0200

    ss

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
-    public class Main {
-        public static void main(String[] args) {
-            Person person = new Person();
-            person.getType(Useradmintype.ADMIN);
-            person.getType(Useradmintype.USER);
-            System.out.println(person);
-            String a = "http://web-for-user";
-
-
+//    public class Main {
+//        public static void main(String[] args) {
+//            Person person = new Person();
+//            person.getType(Useradmintype.ADMIN);
+//            person.getType(Useradmintype.USER);
+//            System.out.println(person);
+//            String a = "http://web-for-user";
+//
+//
 
         }
 
-    }
-}
+
+
--- src/Webfilter.java
+++ src/Webfilter.java
@@ -1,24 +1,24 @@
 
-    public class Webfilter implements Filter{
-
-        @Override
-        public void konnektion() {
-            boolean webadress = Useradmintype.ADMIN;
-            boolean webadress = Useradmintype.USER;
-
-            if (Useradmintype.ADMIN) {
-                System.out.println(webadress);
-            }
-
-            return webadress;
-         else{
-                if (Useradmintype.USER && a.equals(getWebadress())) {
-                    System.out.println(a);
-                }
-
-                return a;
-            }
-
-
-        }
-    }
+//    public class Webfilter implements Filter{
+//
+//        @Override
+//        public void konnektion() {
+//            boolean webadress = Useradmintype.ADMIN;
+//            boolean webadress = Useradmintype.USER;
+//
+//            if (Useradmintype.ADMIN) {
+//                System.out.println(webadress);
+//            }
+//
+//            return webadress;
+//         else{
+//                if (Useradmintype.USER && a.equals(getWebadress())) {
+//                    System.out.println(a);
+//                }
+//
+//                return a;
+//            }
+//
+//
+//        }
+//    }
